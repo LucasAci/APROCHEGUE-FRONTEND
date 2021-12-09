cf7d59b0b62c1fab0a990f7e441b79258750c814
Padilha272 <matheuspadilha48@hotmail.com>
2021-12-09 10:16:21 -0300

    PAdilha272@[Task: Creation of Models from APROCHEGUE]

---
+++ src/app/model/Usuario.ts
@@ -0,0 +1,10 @@
+import { Postagem } from "./Postagem";
+
+export class Usuario{
+public email: string;
+public id: number;
+public nome: string;
+public postagem: Postagem[];
+public senha: string;
+
+}
